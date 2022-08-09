906d3d7010b64ce80cb20be5c663a3fad223fdac
Saman(Lap01) <saman.qaydi@gmail.com>
2022-08-09 20:52:36 +0200

    Creating solution and AbstractionSample

---
+++ src/ObjectOrientedProgrammingPrinciples/AbstractionSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using AbstractionSample;
+
+User validUser = new User
+{
+    Name = "John",
+    Address = "Planet earth"
+};
+
+User invalidUser = new User
+{
+    Name = "J",
+    Address = "TH"
+};
+
+// We can only see the validate method, we hide the parts that are not nessecery.
+validUser.Validate();
+invalidUser.Validate();
+
+
+Console.ReadKey();
---
+++ src/ObjectOrientedProgrammingPrinciples/AbstractionSample/User.cs
@@ -0,0 +1,40 @@
+﻿namespace AbstractionSample;
+
+public class User
+{
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    // Exposing only this method to the client.
+    public void Validate()
+    {
+        ValidateName();
+        ValidateAddress();
+    }
+
+    // There is no reason that client can see this method.
+    private void ValidateName()
+    {
+        if (Name.Length < 3)
+        {
+            Console.WriteLine($"{Name} is not valid");
+        }
+        else
+        {
+            Console.WriteLine($"{Name} is valid");
+        }
+    }
+
+    // There is no reason that client can see this method.
+    private void ValidateAddress()
+    {
+        if (Address.Length < 3)
+        {
+            Console.WriteLine($"{Address} is not valid");
+        }
+        else
+        {
+            Console.WriteLine($"{Address} is valid");
+        }
+    }
+}
